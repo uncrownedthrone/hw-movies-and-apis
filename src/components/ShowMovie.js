9450344c6ab38dd73d3bd9fda991dbf8bc84208f
Justin Swiger <justin.swiger@me.com>
2019-11-21 10:37:10 -0500

    changed from fetch to axios

--- src/components/ShowMovie.js
+++ src/components/ShowMovie.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import MovieData from './MovieData'
+import axios from 'axios'
 
 const MovieAPIInfo = () => {
   const [movies, setMovies] = useState([])
   const getMovieData = async () => {
-    const resp = await fetch(
+    const resp = await axios.get(
       `https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=release_date.asc&api_key=17cb5378f871124dfc852a9d103647e3`
     )
-    const data = await resp.json()
-    console.log(data)
-    setMovies(data.results)
+    console.log(resp)
+    setMovies(
+      resp.data.results.sort((a, b) => {
+        if (a.title > b.title) {
+          return 1
+        } else if (a.title < b.title) {
+          return -1
+        } else {
+          return 0
+        }
+      })
+    )
+    // const data = await resp.json() <-- not needed becuase axios
+    // ASSUMES that the data it's getting back is json unless
+    // you tell it otherwise. fetch doesn't assume anything
   }
 
   useEffect(() => {
